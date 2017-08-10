89ebaf10f7637645f6ce1e5c00b21d0ee39f45da
zam <zam1024t@gmail.com>
2017-08-10 21:47:50 +0800

    Improve push tips

--- push.sh
+++ push.sh
@@ -21,26 +21,26 @@ fi
 lastcommit=`git log -n 1 --pretty=format:"%H"`
 
 # push to oschina
-read -p "Start set statics for oschina... (press any key to continue, wait 9s)" -t 9 -n 1
+read -p "Start set shots for oschina... (press any key to continue, wait 9s)" -t 9 -n 1
 git push osc master -f
 sed -i 's/<br>//g' *.md
 sed -i 's/<br>//g' readme/*.md
 sed -i 's/raw.githubusercontent.com\/zam1024t\/LocalizedMenu/git.oschina.net\/zam1024t\/LocalizedMenu\/raw/g' *.md
 sed -i 's/raw.githubusercontent.com\/zam1024t\/LocalizedMenu/git.oschina.net\/zam1024t\/LocalizedMenu\/raw/g' readme/*.md
-git commit -m"set statics for oschina" .
+git commit -m"set shots for oschina" .
 git push osc master
-read -p "set statics for oschina, Done. (press any key to continue, wait 9s)" -t 9 -n 1
+read -p "set shots for oschina, Done. (press any key to continue, wait 9s)" -t 9 -n 1
 
 # push to coding
 git reset $lastcommit
 git checkout .
-read -p "Start set statics for coding... (press any key to continue, wait 9s)" -t 9 -n 1
+read -p "Start set shots for coding... (press any key to continue, wait 9s)" -t 9 -n 1
 git push coding master -f
 sed -i 's/raw.githubusercontent.com\/zam1024t\/LocalizedMenu/coding.net\/u\/zam1024t\/p\/LocalizedMenu\/git\/raw/g' *.md
 sed -i 's/raw.githubusercontent.com\/zam1024t\/LocalizedMenu/coding.net\/u\/zam1024t\/p\/LocalizedMenu\/git\/raw/g' readme/*.md
-git commit -m"set statics for coding" .
+git commit -m"set shots for coding" .
 git push coding master
-read -p "set statics for coding, Done. (press any key to continue, wait 9s)" -t 9 -n 1
+read -p "set shots for coding, Done. (press any key to continue, wait 9s)" -t 9 -n 1
 
 # clean changes
 git reset $lastcommit
